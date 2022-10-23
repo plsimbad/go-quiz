4d3e4ba48f5d05bafdc60cf44a0a34dfd9d905cd
unknown <241-679-449@web.de>
2022-10-23 20:28:00 +0200

    added timer

--- main.go
+++ main.go
@@ -16,6 +16,7 @@ type questions struct {
 func main() {
 
 	path := flag.String("f", "./problems.csv", "a string")
+	timeLimit := flag.Int("t", 10, "a int")
 	flag.Parse()
 
 	qLst, err := readCSVFile(*path)
@@ -25,18 +26,54 @@ func main() {
 		return
 	}
 
-	correct := 0
-
-	for _, q := range qLst {
-		fmt.Println(q.question)
-		answer := ""
-		fmt.Scanln(&answer)
-		if answer == q.answer {
-			correct++
-		}
-	}
+	correct := askUser(qLst, *timeLimit)
 
 	fmt.Println("answered", correct, "of", len(qLst), "questions")
+}
+
+func askUser(qLst []questions, timeLimit int) int {
+	correct := 0
+	answer := make(chan string)
+	go getInput(answer)
+
+	for _, q := range qLst {
+		timer := time.NewTimer(time.Duration(timeLimit) * time.Second)
+
+		res, err := askQ(q, timer.C, answer)
+		if err != nil {
+			fmt.Println(err)
+			return correct
+		}
+
+		correct += res
+	}
+
+	return correct
+}
+
+func getInput(answer chan string) {
+	for {
+		in := ""
+		fmt.Scanln(&in)
+		answer <- in
+	}
+}
+
+func askQ(question questions, timer <-chan time.Time, answer <-chan string) (int, error) {
+	fmt.Println(question.question)
+	for {
+		select {
+		case <-timer:
+			return 0, fmt.Errorf("Time out")
+		case ans := <-answer:
+			if question.answer == ans {
+				return 1, nil
+			}
+
+			return 0, nil
+		}
+
+	}
 }
 
 func readCSVFile(path string) ([]questions, error) {
